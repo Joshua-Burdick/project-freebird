a671332a78cccaa91b58f86a50b90c4764ac2bad
k1dd <50468072+peter0x7f@users.noreply.github.com>
2024-11-03 20:57:58 -0500

    push

--- test.py
+++ test.py
@@ -1,5 +1,35 @@
 from ultralytics import YOLO
+from PIL import Image
 
-model = YOLO("freebird.pt")
+model = YOLO("yolo11n.pt")
 
-results = model('test-footage-2.mp4', show=True)
+results = model('tank-test.jpg', show=True)
+# Access the first result in the results list and plot the annotated image
+annotated_image = results[0].plot()
+
+# Convert the image to a PIL Image object if needed and save it
+annotated_image_pil = Image.fromarray(annotated_image)
+annotated_image_pil.save("tank-output.jpg")
+
+print("Class indices with average precision:", results.ap_class_index)
+print("Average precision for all classes:", results.box.all_ap)
+print("Average precision:", results.box.ap)
+print("Average precision at IoU=0.50:", results.box.ap50)
+print("Class indices for average precision:", results.box.ap_class_index)
+print("Class-specific results:", results.box.class_result)
+print("F1 score:", results.box.f1)
+print("F1 score curve:", results.box.f1_curve)
+print("Overall fitness score:", results.box.fitness)
+print("Mean average precision:", results.box.map)
+print("Mean average precision at IoU=0.50:", results.box.map50)
+print("Mean average precision at IoU=0.75:", results.box.map75)
+print("Mean average precision for different IoU thresholds:", results.box.maps)
+print("Mean results for different metrics:", results.box.mean_results)
+print("Mean precision:", results.box.mp)
+print("Mean recall:", results.box.mr)
+print("Precision:", results.box.p)
+print("Precision curve:", results.box.p_curve)
+print("Precision values:", results.box.prec_values)
+print("Specific precision metrics:", results.box.px)
+print("Recall:", results.box.r)
+print("Recall curve:", results.box.r_curve)
